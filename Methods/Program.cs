baacfaf32de43f9edc0c81ca25d61f0fa0c80ee7
Evin <dage88397@gmail.com>
2022-08-19 00:04:44 +0300

    notlar

---
+++ Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿Add();
+var result = Add2(35, 52);
+Console.WriteLine(result);
+Console.ReadLine();
+
+static void Add()
+    {
+Console.WriteLine("Metot");
+    }
+
+static int Add2(int n1, int n2)
+{
+    var result= n1 + n2;
+    return result;
+}
+//static int Add2(int n1, int n2=15)
+//default parameter
---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(Add(4, 23, 36, 74, 17, 59));
+Console.ReadLine();
+
+static int Add(params int[] numbers)
+  //  Params: Belirsiz sayıda parametre olduğunda kullanılır
+{
+    return numbers.Sum();
+}
