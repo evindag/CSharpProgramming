b2389c1319d98bada950752db819bb9664e5eb78
evindag <80323753+evindag@users.noreply.github.com>
2022-09-19 21:55:00 +0300

    Classes

---
+++ Classes/Properties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Properties
+    {
+        //fields
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        //encapsulation
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mrs."+_firstName; }
+            set { _firstName = value; }
+        }
+
+        
+
+
+    }
+}
