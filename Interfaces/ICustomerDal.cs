a326e1cfbe0330b655365519a182ffe202f5a02a
evindag <80323753+evindag@users.noreply.github.com>
2022-09-23 18:32:40 +0300

    interface

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Updated");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                Name = "Evin",
+                Address="İstanbul"
+            };
+            Producer producer = new Producer
+            {
+                Id=1,
+                Name="Ayşe",
+                Departmant= "Computing"
+            };
+            personManager.Add(customer);
+            personManager.Add(producer);
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+
+            ICustomerDal[] customerDals = new[] {new SqlServerCustomerDal() };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+            Console.ReadLine();
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+    class Producer : IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Departmant { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+
+}
