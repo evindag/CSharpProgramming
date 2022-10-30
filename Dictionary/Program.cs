1ac36c0001c8ae0a667f1e50d5d3057ae8294eeb
evindag <80323753+evindag@users.noreply.github.com>
2022-10-30 18:12:01 +0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Book", "Kitap");
+            dictionary.Add("Computer", "Bilgisayar");
+            dictionary.Add("Keyboard", "Klavye");
+
+            Console.WriteLine(dictionary["Computer"]);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
