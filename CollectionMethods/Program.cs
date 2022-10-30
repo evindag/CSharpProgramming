cfeab5b5b0d940db6c0851eb84bdbc13afbd9a76
evindag <80323753+evindag@users.noreply.github.com>
2022-10-30 17:01:50 +0300

    Collections

---
+++ CollectionMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("İstanbul");
+
+            Console.WriteLine(cities.Contains("Ankara"));
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, Name = "Ayşe" });
+            customers.Add(new Customer { Id = 2, Name = "Evin" });
+
+           
+
+            customers.AddRange(new Customer[2]
+            {
+                new Customer{Id=3, Name="Ahmet"},
+                new Customer{Id=4, Name="Ali"}
+            });
+            var customer2 = new Customer
+            {
+                Id = 5,
+                Name = "Mehmet"
+            };
+            customers.Insert(0, customer2);
+            customers.RemoveAll(c => c.Name == "Evin");
+            var count = customers.Count();
+            Console.WriteLine("Count : {0}",count);
+            Console.WriteLine(customers.Contains(customer2));
+            
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList cities = new ArrayList();
+            cities.Add("Ankara");
+            cities.Add("Adana");
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            cities.Add("İstanbul");
+            Console.WriteLine(cities[2]);
+            Console.ReadLine();
+        }
+    }
+}
