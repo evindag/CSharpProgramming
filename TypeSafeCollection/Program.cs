a96adcab2eda7360578703801925d65f056dd889
evindag <80323753+evindag@users.noreply.github.com>
2022-10-30 16:36:27 +0300

    TypeSafe

---
+++ TypeSafeCollection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TypeSafeCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("İstanbul");
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, Name = "Ayşe" });
+            customers.Add(new Customer { Id = 2, Name = "Evin" });
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
