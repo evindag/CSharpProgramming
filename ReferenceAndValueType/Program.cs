13b9d46cad71a141a394bed56f2d8e45a98be579
evindag <80323753+evindag@users.noreply.github.com>
2022-10-30 16:36:11 +0300

    ReferenceType

---
+++ ReferenceAndValueType/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReferenceAndValueType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities1 = new string[] { "Ankara", "Adana", "Afyon" };
+            string[] cities2 = new string[] { "Bursa", "Balıkesir", "Bolu" };
+            cities1[0] = "İstanbul";
+            Console.WriteLine(cities2[0]);
+            cities2 = cities1;
+            Console.ReadLine();
+        }
+    }
+}
