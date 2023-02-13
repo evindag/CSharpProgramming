66b9d0c3ade2ee9109b2dab028c4e1fb97b9ff69
evindag <80323753+evindag@users.noreply.github.com>
2023-02-14 00:50:02 +0300

    func

---
+++ Func/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int> getRandomNumber =()=>new Random().Next(1,100); 
+                Console.WriteLine(getRandomNumber());
+        }
+    }
+}
